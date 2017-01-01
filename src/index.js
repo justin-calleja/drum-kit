be06d87241a26f0cddba45acdbde151e5ca52876
Justin Calleja <calleja.justin@gmail.com>
2017-01-01 18:36:23 +0100

    Moved index.js in src/scripts. Added another index.js required by react-scripts which just imports the real index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './scripts/components/App';
+// Do not delete this file. It is required by react-scripts (create-react-app). i.e. auto-config will break if you remove this file.
+// What you *could* do is eject out of react-scripts and change the config.
+// https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#folder-structure
 
-import './styles/index.css';
-
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import './scripts/index';
