d29d32e4dfdd9b8716f94a30fd539e1c76f2abac
Justin Calleja <calleja.justin@gmail.com>
2017-01-01 01:17:16 +0100

    Works with "chords"

--- src/scripts/components/App.js
+++ src/scripts/components/App.js
@@ -14,7 +14,7 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isPlaying: false
+      playBuffer: []
     };
 
     this.keyDown = this.keyDown.bind(this);
@@ -24,13 +24,15 @@ export default class App extends Component {
   transitionEnd(e) {
     if (e.propertyName !== 'transform') return;
 
-    this.setState({ isPlaying: false });
+    this.setState({ playBuffer: [] });
   }
 
   keyDown(e) {
-    this.setState({
-      isPlaying: e.keyCode
-    });
+    if (!this.state.playBuffer.includes(e.keyCode)) {
+      this.setState({
+        playBuffer: this.state.playBuffer.concat([e.keyCode])
+      });
+    }
   }
 
   componentDidMount() {
@@ -44,15 +46,15 @@ export default class App extends Component {
   render() {
     return (
       <div className="keys" onTransitionEnd={this.transitionEnd}>
-        <Clap    keyCode="65" keyChar="A" isPlaying={this.state.isPlaying === 65} />,
-        <HiHat   keyCode="83" keyChar="S" isPlaying={this.state.isPlaying === 83} />,
-        <Kick    keyCode="68" keyChar="D" isPlaying={this.state.isPlaying === 68} />,
-        <OpenHat keyCode="70" keyChar="F" isPlaying={this.state.isPlaying === 70} />,
-        <Boom    keyCode="71" keyChar="G" isPlaying={this.state.isPlaying === 71} />,
-        <Ride    keyCode="72" keyChar="H" isPlaying={this.state.isPlaying === 72} />,
-        <Snare   keyCode="74" keyChar="J" isPlaying={this.state.isPlaying === 74} />,
-        <Tom     keyCode="75" keyChar="K" isPlaying={this.state.isPlaying === 75} />,
-        <Tink    keyCode="76" keyChar="L" isPlaying={this.state.isPlaying === 76} />
+        <Clap    keyCode="65" keyChar="A" isPlaying={this.state.playBuffer.includes(65)} />,
+        <HiHat   keyCode="83" keyChar="S" isPlaying={this.state.playBuffer.includes(83)} />,
+        <Kick    keyCode="68" keyChar="D" isPlaying={this.state.playBuffer.includes(68)} />,
+        <OpenHat keyCode="70" keyChar="F" isPlaying={this.state.playBuffer.includes(70)} />,
+        <Boom    keyCode="71" keyChar="G" isPlaying={this.state.playBuffer.includes(71)} />,
+        <Ride    keyCode="72" keyChar="H" isPlaying={this.state.playBuffer.includes(72)} />,
+        <Snare   keyCode="74" keyChar="J" isPlaying={this.state.playBuffer.includes(74)} />,
+        <Tom     keyCode="75" keyChar="K" isPlaying={this.state.playBuffer.includes(75)} />,
+        <Tink    keyCode="76" keyChar="L" isPlaying={this.state.playBuffer.includes(76)} />
       </div>
     );
   }
